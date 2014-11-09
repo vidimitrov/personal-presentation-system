f0bb9e62cf641817854ed0c7efaadc36a921dc4f
vdimitrov94 <vidimitrov94@gmail.com>
2014-11-10 00:32:56 +0200

    AutoMapper assemblies bug fix

--- Personal-Presentation-System/PPSystem.Web/Global.asax.cs
+++ Personal-Presentation-System/PPSystem.Web/Global.asax.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using PPSystem.Web.Infrastructure.Mapping;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
@@ -18,6 +20,9 @@ namespace PPSystem.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var autoMapperConfig = new AutoMapperConfig(Assembly.GetExecutingAssembly());
+            autoMapperConfig.Execute();
         }
     }
 }
