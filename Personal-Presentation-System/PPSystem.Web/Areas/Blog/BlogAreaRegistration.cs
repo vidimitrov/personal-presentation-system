4b506485201b94f1492786f44960942145bd1499
vdimitrov94 <vidimitrov94@gmail.com>
2014-11-13 23:56:12 +0200

    Blog moved to a separate area
    Basic configuration added for Blog system in a separate area

---
+++ Personal-Presentation-System/PPSystem.Web/Areas/Blog/BlogAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace PPSystem.Web.Areas.Blog
+{
+    public class BlogAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Blog";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Blog_default",
+                "Blog/{action}/{id}",
+                new { controller = "Blog", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Personal-Presentation-System/PPSystem.Web/Areas/Blog/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿namespace PPSystem.Web.Areas.Blog.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Blog/Manage
+        public ActionResult Manage()
+        {
+            return View();
+        }
+    }
+}
