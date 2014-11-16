23c3d5281f09532dc0dcaee8feb50ff08e84d2fb
vidimitrov <vidimitrov94@gmail.com>
2014-11-16 16:22:28 +0200

    View for CV manager added. Views for previous experience and languages managers added. Bootstrap grid added as partial.

--- Personal-Presentation-System/PPSystem.Web/Areas/Administration/Controllers/ManageCVController.cs
+++ Personal-Presentation-System/PPSystem.Web/Areas/Administration/Controllers/ManageCVController.cs
@@ -1,16 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace PPSystem.Web.Areas.Administration.Controllers
+﻿namespace PPSystem.Web.Areas.Administration.Controllers
 {
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
+    using PPSystem.Common.Repository;
+    using PPSystem.Models.CV;
+    using PPSystem.Web.ViewModels.CV;
+    using System;
+
     public class ManageCVController : Controller
     {
+        private readonly IDeletableEntityRepository<CV> cvs;
+        private readonly IDeletableEntityRepository<Experience> experiences;
+
+        public ManageCVController(IDeletableEntityRepository<CV> cvs, IDeletableEntityRepository<Experience> experiences)
+        {
+            this.cvs = cvs;
+            this.experiences = experiences;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var cv = this.cvs.All().Project().To<CvViewModel>().FirstOrDefault();
+
+            return View(cv);
         }
 
         public ActionResult ManageSkills()
@@ -30,12 +45,95 @@ namespace PPSystem.Web.Areas.Administration.Controllers
 
         public ActionResult ManageLanguages()
         {
+            var cv = this.cvs.All().Project().To<CvViewModel>().FirstOrDefault();
+            var languages = cv.Languages;
+            
+            return View(languages);
+        }
+
+        public ActionResult AddLanguage()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddLanguage(LanguageViewModel inputLanguage)
+        {
+            var cv = this.cvs.All().FirstOrDefault();
+                        
+            if (ModelState.IsValid)
+            {
+                var language = new Language()
+                {
+                    Name = inputLanguage.Name
+                };
+
+                cv.Languages.Add(language);
+                
+                this.cvs.SaveChanges();
+            }
+
+            return RedirectToAction("Index", "ManageCV");
         }
 
         public ActionResult ManagePreviousExperience()
         {
+            var cv = this.cvs.All().Project().To<CvViewModel>().FirstOrDefault();
+            var experiences = cv.Experiences;
+            
+            return View(experiences);
+        }
+
+        public ActionResult AddExperience()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddExperience(ExperienceViewModel inputExperience)
+        {
+            var cv = this.cvs.All().FirstOrDefault();
+
+            var dateFrom = ParseStringToDate(Request["date-from"]);
+            var dateTo = ParseStringToDate(Request["date-to"]);
+            
+            if (ModelState.IsValid)
+            {
+                var experience = new Experience()
+                {
+                    CompanyName = ,
+                    CreatedOn = DateTime.Now,
+                    Description = inputExperience.Description,
+                    From = dateFrom,
+                    To = dateTo,
+                    JobTitle = inputExperience.JobTitle,
+                    Type = inputExperience.Type
+                };
+
+                cv.Experiences.Add(experience);
+
+                this.cvs.SaveChanges();
+            }
+
+            return RedirectToAction("Index", "ManageCV");
+        }
+
+        private DateTime? ParseStringToDate(string dateString)
+        {
+            if (dateString == string.Empty) 
+            {
+                return null;
+            }
+
+            var splittedDate = dateString.Split('/');
+
+            var dateToDay = int.Parse(splittedDate[0]);
+            var dateToMonth = int.Parse(splittedDate[1]);
+            var dateToYear = int.Parse(splittedDate[2]);
+
+            var parsedDate = new DateTime(dateToYear, dateToMonth, dateToDay);
+
+            return parsedDate;
         }
     }
 }
