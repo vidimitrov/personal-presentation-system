ac132b560cea7a48c57ae5434c201b4b0d59f546
vidimitrov <vidimitrov94@gmail.com>
2014-11-16 21:48:05 +0200

    Administration view for preview all projects added. Add project functionality added.

--- Personal-Presentation-System/PPSystem.Web/Areas/Administration/Controllers/ManagePortfolioController.cs
+++ Personal-Presentation-System/PPSystem.Web/Areas/Administration/Controllers/ManagePortfolioController.cs
@@ -1,17 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace PPSystem.Web.Areas.Administration.Controllers
+﻿namespace PPSystem.Web.Areas.Administration.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+    
+    using PPSystem.Common.Repository;
+    using PPSystem.Models.Portfolio;
+    using PPSystem.Web.ViewModels.Portfolio;
+
     public class ManagePortfolioController : Controller
     {
-        // GET: Administration/Projects
+        private readonly IDeletableEntityRepository<Portfolio> portfolios;
+        private readonly IDeletableEntityRepository<Project> projects;
+
+        public ManagePortfolioController(IDeletableEntityRepository<Project> projects,
+            IDeletableEntityRepository<Portfolio> portfolios)
+        {
+            this.projects = projects;
+            this.portfolios = portfolios;
+        }
+
         public ActionResult Index()
         {
+            var portfolio = this.portfolios.All().Project().To<PortfolioViewModel>().FirstOrDefault();
+
+            if (portfolio == null) 
+            {
+                var initialPortfolio = new Portfolio();
+
+                this.portfolios.Add(initialPortfolio);
+                this.portfolios.SaveChanges();
+            }
+
+            var projects = this.projects.All().Project().To<ProjectViewModel>().ToList();
+
+            return View(projects);
+        }
+
+        public ActionResult AddProject()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddProject(ProjectViewModel inputProject)
+        {
+            var portfolio = this.portfolios.All().FirstOrDefault();
+                        
+            if (ModelState.IsValid)
+            {
+                var project = new Project()
+                {
+                    Title = inputProject.Title,
+                    Description = inputProject.Description,
+                    Rating = 0,
+                    RepoUrl = inputProject.RepoUrl,
+                    CreatedOn = DateTime.Now,
+                    Image = inputProject.Image
+                };
+
+                portfolio.Projects.Add(project);
+                
+                this.portfolios.SaveChanges();
+            }
+
+            return RedirectToAction("Index", "ManagePortfolio");
         }
     }
 }
--- Personal-Presentation-System/PPSystem.Web/Controllers/PortfolioController.cs
+++ Personal-Presentation-System/PPSystem.Web/Controllers/PortfolioController.cs
@@ -1,16 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace PPSystem.Web.Controllers
+﻿namespace PPSystem.Web.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using AutoMapper.QueryableExtensions;
+
+    using PPSystem.Common.Repository;
+    using PPSystem.Models.Portfolio;
+    using PPSystem.Web.ViewModels.Portfolio;
+
     public class PortfolioController : Controller
     {
-        // GET: Portfolio
+        private readonly IDeletableEntityRepository<Portfolio> portfolios;
+
+        public PortfolioController(IDeletableEntityRepository<Portfolio> portfolios)
+        {
+            this.portfolios = portfolios;
+        }
+
         public ActionResult Index()
         {
+            var portfolio = this.portfolios.All().Project().To<PortfolioViewModel>().FirstOrDefault();
+
+            if (portfolio == null) 
+            {
+                var initialPortfolio = new Portfolio();
+
+                this.portfolios.Add(initialPortfolio);
+                this.portfolios.SaveChanges();
+            }
+
             return View();
         }
     }
