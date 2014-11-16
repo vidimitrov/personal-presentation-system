3634875a5aa9835099acab7f6214c869c5022489
vidimitrov <vidimitrov94@gmail.com>
2014-11-16 16:16:49 +0200

    KendoUI libraries added. Bundles refactored and separated into script and style bundles.

--- Personal-Presentation-System/PPSystem.Web/App_Start/BundleConfig.cs
+++ Personal-Presentation-System/PPSystem.Web/App_Start/BundleConfig.cs
@@ -8,28 +8,47 @@ namespace PPSystem.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.IgnoreList.Clear();
+
+            RegisterScriptBundles(bundles);
+            RegisterStyleBundles(bundles);
+
+            BundleTable.EnableOptimizations = false;
+        }
+        private static void RegisterStyleBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo").Include(
+                "~/Content/kendo/kendo.common.min.css",
+                "~/Content/kendo/kendo.common-bootstrap.min.css",
+                "~/Content/kendo/kendo.silver.min.css"));
+
+            bundles.Add(new StyleBundle("~/Content/custom").Include(
+                "~/Content/Site.css"));
+        }
+        private static void RegisterScriptBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/kendo").Include(
+                "~/Scripts/kendo/kendo.all.min.js",
+                "~/Scripts/kendo/kendo.aspnetmvc.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/kendo/jquery.min.js"));
+                //.Include("~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"));
+                // Use the development version of Modernizr to develop with and learn from. Then, when you're
+                // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
         }
     }
 }
