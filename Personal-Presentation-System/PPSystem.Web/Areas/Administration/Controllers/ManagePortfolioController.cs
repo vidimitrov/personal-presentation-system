6181eea8be88e9b4ecf40431dbb42fb9965648ad
vidimitrov <vidimitrov94@gmail.com>
2014-11-16 23:54:33 +0200

    Editing project almost implemented. The views and the deletion functionality are ready.

--- Personal-Presentation-System/PPSystem.Web/Areas/Administration/Controllers/ManagePortfolioController.cs
+++ Personal-Presentation-System/PPSystem.Web/Areas/Administration/Controllers/ManagePortfolioController.cs
@@ -11,17 +11,24 @@
     using PPSystem.Common.Repository;
     using PPSystem.Models.Portfolio;
     using PPSystem.Web.ViewModels.Portfolio;
+    using System.Data;
 
     public class ManagePortfolioController : Controller
     {
         private readonly IDeletableEntityRepository<Portfolio> portfolios;
         private readonly IDeletableEntityRepository<Project> projects;
+        private readonly IDeletableEntityRepository<Technology> technologies;
+        private readonly IDeletableEntityRepository<Contributor> contributors;
 
         public ManagePortfolioController(IDeletableEntityRepository<Project> projects,
-            IDeletableEntityRepository<Portfolio> portfolios)
+            IDeletableEntityRepository<Portfolio> portfolios,
+            IDeletableEntityRepository<Technology> technologies,
+            IDeletableEntityRepository<Contributor> contributors)
         {
             this.projects = projects;
             this.portfolios = portfolios;
+            this.technologies = technologies;
+            this.contributors = contributors;
         }
 
         public ActionResult Index()
@@ -67,6 +74,114 @@
                 
                 this.portfolios.SaveChanges();
             }
+
+            return RedirectToAction("Index", "ManagePortfolio");
+        }
+
+        public ActionResult DeleteProject(int id)
+        {
+            this.projects.Delete(this.projects.GetById(id));
+            this.projects.SaveChanges();
+
+            return RedirectToAction("Index", "ManagePortfolio");
+        }
+
+        public ActionResult EditProject(int id)
+        {
+            var project = this.projects.All()
+                .Project()
+                .To<ProjectViewModel>()
+                .Where(e => e.Id == id)
+                .FirstOrDefault();
+
+            return View(project);
+        }
+
+        [HttpPost]
+        public ActionResult EditProject(ProjectViewModel editedProject)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var project = this.projects.All().Where(u => u.Id == editedProject.Id).First();
+                    
+                    if (editedProject.Image != null && editedProject.Image != project.Image) 
+                    {
+                        project.Image = editedProject.Image;
+                    }
+
+                    if (editedProject.Rating != null && editedProject.Rating != project.Rating) 
+                    {
+                        project.Rating = editedProject.Rating;
+                    }
+
+                    if (editedProject.RepoUrl != null && editedProject.RepoUrl != project.RepoUrl) 
+                    {
+                        project.RepoUrl = editedProject.RepoUrl;
+                    }
+
+                    if (editedProject.Title != null && editedProject.Title != project.Title) 
+                    {
+                        project.Title = editedProject.Title;
+                    }
+
+                    if (editedProject.Description != null && editedProject.Description != project.Description) 
+                    {
+                        project.Description = editedProject.Description;
+                    }
+
+                    this.projects.SaveChanges();
+
+                    return RedirectToAction("Index", "ManagePortfolio");
+                }
+            }
+            catch (DataException)
+            {
+                ModelState.AddModelError("", "Unable to save project changes.");
+            }
+
+            return View();
+        }
+
+        public ActionResult AddTechnology()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddTechnology(TechnologyViewModel inputTechnology)
+        {
+            // Implement this
+
+            return View();
+        }
+
+        public ActionResult DeleteTechnology(int id)
+        {
+            this.technologies.Delete(this.technologies.GetById(id));
+            this.technologies.SaveChanges();
+
+            return RedirectToAction("Index", "ManagePortfolio");
+        }
+
+        public ActionResult AddContributor()
+        {
+            // Implement this
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddContributor(ContributorViewModel inputContributor)
+        {
+            return View();
+        }
+
+        public ActionResult DeleteContributor(int id)
+        {
+            this.contributors.Delete(this.contributors.GetById(id));
+            this.contributors.SaveChanges();
 
             return RedirectToAction("Index", "ManagePortfolio");
         }
