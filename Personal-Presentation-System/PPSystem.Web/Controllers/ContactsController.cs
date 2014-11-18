55ee6a8d6f47c74cc77ae7372e41e36395eb058d
vidimitrov <vidimitrov94@gmail.com>
2014-11-18 10:25:15 +0200

    Contacts view added and changes on home page

--- Personal-Presentation-System/PPSystem.Web/Controllers/ContactsController.cs
+++ Personal-Presentation-System/PPSystem.Web/Controllers/ContactsController.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace PPSystem.Web.Controllers
+﻿namespace PPSystem.Web.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    using Postal;
+    using PPSystem.Web.ViewModels.Contacts;
+    using PPSystem.Web.Models.Emails;
+
     public class ContactsController : Controller
     {
-        // GET: Contacts
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SendMail(EmailViewModel inputEmail)
+        {
+            if (ModelState.IsValid)
+            {
+                var email = new ContactEmail
+                {
+                    To = "vidimitrov94@gmail.com", //this.User.Identity.Name,
+                    From = inputEmail.EmailFrom,
+                    Message = inputEmail.Content
+                };
+
+                email.Send();
+            }
+
+            return RedirectToAction("Index", "Contacts");
         }
     }
 }
