c78bfd19824c37b90178a6088dde16856c095949
vidimitrov <vidimitrov94@gmail.com>
2014-11-18 10:25:39 +0200

    Postal package installed for sending email

---
+++ Personal-Presentation-System/PPSystem.Web/Models/Emails/ContactEmail.cs
@@ -0,0 +1,13 @@
+﻿namespace PPSystem.Web.Models.Emails
+{
+    using Postal;
+
+    public class ContactEmail : Email
+    {
+        public string To { get; set; }
+
+        public string From { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ Personal-Presentation-System/PPSystem.Web/ViewModels/Contacts/EmailViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace PPSystem.Web.ViewModels.Contacts
+{
+    public class EmailViewModel
+    {
+        [DisplayName("Your email")]
+        public string EmailFrom { get; set; }
+        
+        [DisplayName("Your message")]
+        public string Content { get; set; }
+    }
+}
